43ce80641a3339389047201c5ebce2538a3b67a3
laotang <laotangzaizheli@aliyun.com>
2021-07-22 11:35:35 +0800

    网络图片加载策略修改

--- carryLibrary/src/main/java/me/laotang/carry/core/imageloader/ImageLoaderViewTarget.kt
+++ carryLibrary/src/main/java/me/laotang/carry/core/imageloader/ImageLoaderViewTarget.kt
@@ -1,22 +1,20 @@
 package me.laotang.carry.core.imageloader
 
 import android.graphics.drawable.Drawable
-import android.net.Uri
-import java.io.File
 
-open class ImageLoaderViewTarget<T>(val scr: T) {
-    var placeholder: Int = 0
-    var errorPic: Int = 0
-    var isCenterCrop: Boolean = false
-    var isCircle: Boolean = false
-    var imageRadius: Int = 0
-    var targetWidth: Int = 0
-    var targetHeight: Int = 0
 
-    //0对应DiskCacheStrategy.all,1对应DiskCacheStrategy.NONE,2对应DiskCacheStrategy.SOURCE,3对应DiskCacheStrategy.RESULT
-    var cacheStrategy: Int = 0
-
-    var crossFade: Boolean = false
+open class ImageLoaderViewTarget<T>(
+    val scr: T,
+    val placeholder: Int = 0,
+    val errorPic: Int = 0,
+    val isCenterCrop: Boolean = false,
+    val isCircle: Boolean = false,
+    val imageRadius: Int = 0,
+    val targetWidth: Int = 0,
+    val targetHeight: Int = 0,
+    val cacheStrategy: Int = 0,
+    val crossFade: Boolean = false
+) {
 
     fun isImageRadius(): Boolean {
         return imageRadius > 0
@@ -35,57 +33,56 @@ open class ImageLoaderViewTarget<T>(val scr: T) {
     }
 }
 
-open class UrlImageLoaderViewTarget(url: String) : ImageLoaderViewTarget<String>(url) {
-    open fun copy(block: UrlImageLoaderViewTarget.() -> Unit): UrlImageLoaderViewTarget {
-        val target =
-            UrlImageLoaderViewTarget(scr)
-        target.block()
-        return target
+open class UrlImageLoaderViewTarget(
+    url: String,
+    placeholder: Int = 0,
+    errorPic: Int = 0,
+    isCenterCrop: Boolean = false,
+    isCircle: Boolean = false,
+    imageRadius: Int = 0,
+    targetWidth: Int = 0,
+    targetHeight: Int = 0,
+    cacheStrategy: Int = 0,
+    crossFade: Boolean = false
+) : ImageLoaderViewTarget<String>(
+    url,
+    placeholder,
+    errorPic,
+    isCenterCrop,
+    isCircle,
+    imageRadius,
+    targetWidth,
+    targetHeight,
+    cacheStrategy,
+    crossFade
+) {
+    open fun copy(
+        scr: String? = null,
+        placeholder: Int? = null,
+        errorPic: Int? = null,
+        isCenterCrop: Boolean? = null,
+        isCircle: Boolean? = null,
+        imageRadius: Int? = null,
+        targetWidth: Int? = null,
+        targetHeight: Int? = null,
+        cacheStrategy: Int? = null,
+        crossFade: Boolean? = null
+    ): UrlImageLoaderViewTarget {
+        return UrlImageLoaderViewTarget(
+            url = scr ?: this.scr,
+            placeholder = placeholder ?: this.placeholder,
+            errorPic = errorPic ?: this.errorPic,
+            isCenterCrop = isCenterCrop ?: this.isCenterCrop,
+            isCircle = isCircle ?: this.isCircle,
+            imageRadius = imageRadius ?: this.imageRadius,
+            targetWidth = targetWidth ?: this.targetWidth,
+            targetHeight = targetHeight ?: this.targetHeight,
+            cacheStrategy = cacheStrategy ?: this.cacheStrategy,
+            crossFade = crossFade ?: this.crossFade,
+        )
     }
-}
-
-open class FileImageLoaderViewTarget(file: File) : ImageLoaderViewTarget<File>(file) {
-    open fun copy(block: FileImageLoaderViewTarget.() -> Unit): FileImageLoaderViewTarget {
-        val target =
-            FileImageLoaderViewTarget(
-                scr
-            )
-        target.block()
-        return target
-    }
-}
-
-open class UriImageLoaderViewTarget(uri: Uri) : ImageLoaderViewTarget<Uri>(uri) {
-    open fun copy(block: UriImageLoaderViewTarget.() -> Unit): UriImageLoaderViewTarget {
-        val target =
-            UriImageLoaderViewTarget(scr)
-        target.block()
-        return target
-    }
-}
-
-open class ResImageLoaderViewTarget(resId: Int) : ImageLoaderViewTarget<Int>(resId) {
-    open fun copy(block: ResImageLoaderViewTarget.() -> Unit): ResImageLoaderViewTarget {
-        val target =
-            ResImageLoaderViewTarget(scr)
-        target.block()
-        return target
-    }
-}
-
-inline fun <T> ImageLoaderViewTarget<T>.config(block: ImageLoaderViewTarget<T>.() -> Unit): ImageLoaderViewTarget<T> {
-    this.block()
-    return this
 }
 
 fun String.toImageLoaderViewTarget(): ImageLoaderViewTarget<String> {
     return UrlImageLoaderViewTarget(this)
-}
-
-fun File.toImageLoaderViewTarget(): ImageLoaderViewTarget<File> {
-    return FileImageLoaderViewTarget(this)
-}
-
-fun Uri.toImageLoaderViewTarget(): ImageLoaderViewTarget<Uri> {
-    return UriImageLoaderViewTarget(this)
 }
